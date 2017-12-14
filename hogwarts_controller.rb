24e3a8c24e26c0c22d4d05c61406373fff961063
Dave Ellis <dave@ellisnetmail.co.uk>
2017-12-14 11:24:50 +0000

    added new student form

--- hogwarts_controller.rb
+++ hogwarts_controller.rb
@@ -7,3 +7,17 @@ get('/hogwarts') do
   @students = Student.all()
   erb(:index)
 end
+
+#New
+
+get('/hogwarts/new') do
+  erb(:new)
+end
+
+#Create
+
+post('/hogwarts') do
+  @student = Student.new(params)
+  @student.save
+  redirect('/hogwarts')
+end
