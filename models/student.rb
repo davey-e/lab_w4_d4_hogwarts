a6fc2ecdeaa2db4584598d936a728f9db00e70f3
Dave Ellis <dave@ellisnetmail.co.uk>
2017-12-14 12:08:40 +0000

    added foreign key for students class

--- models/student.rb
+++ models/student.rb
@@ -3,21 +3,21 @@ require_relative('../db/sql_runner.rb')
 
 class Student
 
-  attr_reader :id, :first_name, :last_name, :house, :age
+  attr_reader :id, :first_name, :last_name, :house_id, :age
 
   def initialize (options)
     @id = options['id'].to_i
     @first_name = options['first_name']
     @last_name = options['last_name']
-    @house = options['house']
+    @house_id = options['house_id'].to_i
     @age = options['age'].to_i
   end
 
   def save()
-    sql = "INSERT INTO students (first_name, last_name, house, age)
+    sql = "INSERT INTO students (first_name, last_name, house_id, age)
     VALUES ($1, $2, $3, $4)
     RETURNING id"
-    values = [@first_name, @last_name, @house, @age]
+    values = [@first_name, @last_name, @house_id, @age]
     student = SqlRunner.run(sql, values).first
     @id = student['id'].to_i
   end
