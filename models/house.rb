341fc82e336ab462d38fd933078fe49ab1bb3395
Dave Ellis <dave@ellisnetmail.co.uk>
2017-12-14 11:47:27 +0000

    Added self.all and find methods to House class

--- models/house.rb
+++ models/house.rb
@@ -2,25 +2,41 @@ require_relative('../db/sql_runner.rb')
 
 class House
 
-   attr_reader :id, :name
+  attr_reader :id, :name
 
-   def initialize(options)
-     @id = options['id'].to_i
-     @name = options['name']
-   end
+  def initialize(options)
+    @id = options['id'].to_i
+    @name = options['name']
+  end
 
-   def save()
-     sql = "INSERT INTO houses (name)
-     VALUES ($1)
-     RETURNING id;"
-     values = [@name]
-     house = SqlRunner.run(sql, values).first()
-     @id = house['id'].to_i
-   end
+  def save()
+    sql = "INSERT INTO houses (name)
+    VALUES ($1)
+    RETURNING id;"
+    values = [@name]
+    house = SqlRunner.run(sql, values).first()
+    @id = house['id'].to_i
+  end
 
-   def self.delete_all()
-     sql = "DELETE FROM houses"
-     SqlRunner.run(sql)
-   end
+  def find()
+    sql = "SELECT * FROM houses
+    WHERE id = $1"
+    values =[@id]
+    house_hash = SqlRunner.run(sql, values).first()
+    return House.new(house_hash)
+  end
 
- end
+  def self.delete_all()
+    sql = "DELETE FROM houses;"
+    SqlRunner.run(sql)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM houses;"
+    house_hashes = SqlRunner.run(sql)
+    result = house_hashes.map {|house| House.new(house)}
+    return result
+  end
+
+
+end
